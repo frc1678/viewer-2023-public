12d1d24a78740a00593898abb17e1c511e134b94
ekim1678 <43869790+ekim1678@users.noreply.github.com>
2021-10-28 19:43:27 -0700

    Add vertical lines to Match Details cell (#182)

--- app/src/main/java/com/example/viewer_2020/constants/Constants.kt
+++ app/src/main/java/com/example/viewer_2020/constants/Constants.kt
@@ -91,7 +91,7 @@ class Constants {
         const val EMPTY_CHARACTER = ""
         const val RANKING_POINT_CHARACTER = "•"
         const val PREDICTED_RANKING_POINT_QUALIFICATION = 0.65
-        const val VERSION_NUM = "1.3.2"
+        const val VERSION_NUM = "1.3.3"
     }
 
     enum class PROCESSED_OBJECT(val value: String) {
