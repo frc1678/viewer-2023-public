95c4922485f60b640651340d7248df69a5851938
elliekim <ellie.yso.kim@gmail.com>
2022-01-17 14:18:18 -0800

    update datapoints for team

--- app/src/main/java/com/example/viewer_2020/data/Team.kt
+++ app/src/main/java/com/example/viewer_2020/data/Team.kt
@@ -10,13 +10,7 @@ package com.example.viewer_2020.data
 
 // Data storage class for each individual team object.
 data class Team (var team_number: Int?) {
-    var auto_high_balls_percent_inner: Float? = null
-    var tele_high_balls_percent_inner: Float? = null
-    var climb_all_success_avg_time: Float? = null
     var team_name: String? = null
-    var climb_all_successes: Int? = null
-    var climb_level_successes: Int? = null
-    var park_successes: Int? = null
     var auto_line_successes: Int? = null
     var first_pickability: Float? = null
     var second_pickability: Float? = null
@@ -26,7 +20,8 @@ data class Team (var team_number: Int?) {
     var current_rank: Int? = null
     var current_avg_rps: Float? = null
     var driver_quickness: Float? = null
-    var driver_field_awareness: Float? = null
+    var driver_far_field_awareness: Float? = null
+    var driver_near_field_awareness: Float? = null
     var driver_ability: Float? = null
     var auto_avg_balls_low: Float? = null
     var auto_avg_balls_high: Float? = null
@@ -34,11 +29,7 @@ data class Team (var team_number: Int?) {
     var tele_avg_balls_low: Float? = null
     var tele_avg_balls_high: Float? = null
     var tele_avg_balls_total: Float? = null
-    var tele_cp_rotation_successes: Int? = null
-    var tele_cp_position_successes: Int? = null
     var climb_all_attempts: Int? = null
-    var climber_strap_installation_notes: String? = null
-    var climber_strap_installation_difficulty: Int? = null
     var avg_incap_time: Float? = null
     var climb_percent_success: Float? = null
     var match_number: Int? = null
@@ -46,8 +37,6 @@ data class Team (var team_number: Int?) {
     var auto_balls_high: Int? = null
     var tele_balls_low: Int? = null
     var tele_balls_high: Int? = null
-    var control_panel_rotation: Boolean? = null
-    var control_panel_position: Boolean? = null
     var incap: Int? = null
     var climb_time: Int? = null
     var auto_max_balls_low: Int? = null
@@ -55,4 +44,34 @@ data class Team (var team_number: Int?) {
     var tele_max_balls_low: Int? = null
     var tele_max_balls_high: Int? = null
     var max_incap: Int? = null
+    var auto_sd_balls_low: Float? = null
+    var auto_sd_balls_high: Float? = null
+    var mode_start_position: String? = null
+    var tele_sd_balls_low: Float? = null
+    var tele_sd_balls_high: Float? = null
+    var low_rung_successes: Int? = null
+    var mid_rung_successes: Int? = null
+    var high_rung_successes: Int? = null
+    var traversal_rung_successes: Int? = null
+    var matches_incap: Int? = null
+    var mode_climb_level: String? = null
+    var max_climb_level: String? = null
+    var low_avg_time: Float? = null
+    var mid_avg_time: Float? = null
+    var high_avg_time: Float? = null
+    var traversal_avg_time: Float? = null
+    var avg_exit_ball_catches: Float? = null
+    var max_exit_ball_catches: Int? = null
+    var avg_opp_balls_scored: Float? = null
+    var max_opp_balls_scored: Int? = null
+    var drivetrain: Int? = null
+    var drivetrain_motors: Int? = null
+    var drivetrain_motor_type: Int? = null
+    var can_climb: Boolean? = null
+    var can_ground_intake: Boolean? = null
+    var can_intake_terminal: Boolean? = null
+    var can_eject_terminaal: Boolean? = null
+    var has_vision: Boolean? = null
+    var can_cheesecake: Boolean? = null
+    var can_under_low_rung: Boolean? = null
 }
