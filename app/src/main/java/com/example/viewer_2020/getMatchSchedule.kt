055454c0149d7520e70895aeb60694bd3cfc2701
NathanXMills <nathan.x.mills@gmail.com>
2022-02-06 13:10:40 -0800

    search in starred matches

--- app/src/main/java/com/example/viewer_2020/getMatchSchedule.kt
+++ app/src/main/java/com/example/viewer_2020/getMatchSchedule.kt
@@ -1,29 +1,42 @@
 package com.example.viewer_2020
 
+import android.util.Log
 import com.example.viewer_2020.data.Match
 import com.example.viewer_2020.data.WebsiteMatchSchedule
 
 fun getMatchSchedule(teamNumber: String? = null, starred: Boolean = false): Map<String, Match> {
     if (starred) {
         val tempMatches = mutableMapOf<String, Match>()
+        val staredMatches = mutableMapOf<String, Match>()
+        val returnedMatches = mutableMapOf<String, Match>()
         for (i in MainViewerActivity.matchCache) {
             if (MainViewerActivity.starredMatches.contains(i.value.matchNumber)) {
                 tempMatches[i.key] = i.value
             }
         }
-        //Nathan, this is the part that is broken
         if (teamNumber != null) {
-            for (i in tempMatches) {
-                if (teamNumber in i.value.redTeams || teamNumber in i.value.blueTeams) {
-                    tempMatches[i.key] = i.value
+            for (i in MainViewerActivity.matchCache) {
+                if ((teamNumber in i.value.redTeams) or (teamNumber in i.value.blueTeams)) {
+                    staredMatches[i.key] = i.value
                 }
             }
+            for (i in tempMatches) {
+                for (x in staredMatches) {
+                    if (i == x) {
+                        returnedMatches[x.key] = x.value
+                    }
+                }
+            }
+            return returnedMatches
         }
-        return tempMatches
+        else {
+            return tempMatches
+        }
+
     } else if (teamNumber != null) {
         val tempMatches = mutableMapOf<String, Match>()
         for (i in MainViewerActivity.matchCache) {
-            if (teamNumber in i.value.redTeams || teamNumber in i.value.blueTeams) {
+            if ((teamNumber in i.value.redTeams) or (teamNumber in i.value.blueTeams)) {
                 tempMatches[i.key] = i.value
             }
         }
