9e79efcf8ee519af9ef01f294316a21684e485ff
Cyan <blueplus.d@gmail.com>
2022-02-12 15:01:57 -0800

    moved version number to Preferences

--- app/src/main/java/com/example/viewer_2020/PreferencesFragment.kt
+++ app/src/main/java/com/example/viewer_2020/PreferencesFragment.kt
@@ -8,19 +8,24 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import kotlinx.android.synthetic.main.fragment_preferences.*
 import kotlinx.android.synthetic.main.fragment_preferences.view.*
 import com.example.viewer_2020.MainViewerActivity.UserDatapoints
-
+import com.example.viewer_2020.constants.Constants
 class PreferencesFragment: IFrag() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
 
         val root = inflater.inflate(R.layout.fragment_preferences, container, false)
+        val versionNumber = this.getString(R.string.tv_version_num, Constants.VERSION_NUM)
+        root.tv_version_num.text = versionNumber
         context?.let { createSpinner(it, root.spin_user, R.array.user_array) }
 
         val name = UserDatapoints.contents?.get("selected")?.asString?.toLowerCase()?.capitalize()
@@ -67,6 +72,7 @@ class PreferencesFragment: IFrag() {
                 return
             }
         }
+
 
     }
 }
