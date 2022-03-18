aa34cfdf2ee618849dc1965f5d21df2b953dc369
Alison Lin <totorodog576@gmail.com>
2022-03-17 18:47:41 -0700

    bold the winning alliance

--- app/src/main/java/com/example/viewer_2022/constants/Constants.kt
+++ app/src/main/java/com/example/viewer_2022/constants/Constants.kt
@@ -15,7 +15,7 @@ class Constants {
         const val EVENT_KEY = "2022caph"
         const val MONGO_ATLAS = "mongodb-atlas"
         const val MY_TEAM_NUMBER = "1678"
-        const val USE_TEST_DATA = false
+        const val USE_TEST_DATA = true
         const val CARDINAL_KEY = "6260ddb0f1414290375cc01f0d7739d79149ac9c"
         const val REFRESH_INTERVAL = 180
         //In order to change the URL, see the GeDataFromWebsite Async Task
--- app/src/main/java/com/example/viewer_2022/data/DatabaseReference.kt
+++ app/src/main/java/com/example/viewer_2022/data/DatabaseReference.kt
@@ -49,7 +49,8 @@ class DatabaseReference {
         var actual_rp2: Float,
         var predicted_score: Float,
         var predicted_rp1: Float,
-        var predicted_rp2: Float
+        var predicted_rp2: Float,
+        var won_match: Boolean
     )
 
     data class CalculatedTBATeam (
