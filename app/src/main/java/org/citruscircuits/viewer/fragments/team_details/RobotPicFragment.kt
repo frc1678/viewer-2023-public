6b4625d970bd151c5e1a60720335bcdf9e5220de
totorodog576@gmail.com <totorodog576@gmail.com>
2023-03-15 19:00:31 -0700

    add all pictures to Viewer

--- app/src/main/java/org/citruscircuits/viewer/fragments/team_details/RobotPicFragment.kt
+++ app/src/main/java/org/citruscircuits/viewer/fragments/team_details/RobotPicFragment.kt
@@ -20,11 +20,17 @@ import java.io.File
 
 /**
  * Page for showing robot pictures
+ * Navigate to this page by clicking on the Picture button in the top right of Team Details
+ * The Picture button will only appear if the phone has a picture for the robot
+ * Pictures are stored in the file path:
+ * storage -> self -> primary -> Android -> data -> com.example.viewer -> files
  */
 class RobotPicFragment : Fragment() {
     private var teamNumber: String? = null
     private var teamName: String? = null
-    private var picFile: File? = null
+    private var picFileFull: File? = null
+    private var picFileFront: File? = null
+    private var picFileSide: File? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -36,6 +42,7 @@ class RobotPicFragment : Fragment() {
         return root
     }
 
+    // Sets the image header for the team number and name
     private fun populateTeamInfo(root: View) {
         teamNumber = arguments?.getString(Constants.TEAM_NUMBER, Constants.NULL_CHARACTER)
         teamName = getTeamDataValue(teamNumber!!, "team_name")
@@ -43,18 +50,33 @@ class RobotPicFragment : Fragment() {
         root.tv_team_name.text = teamName.toString()
     }
 
+    // Gets the picture files from the phone's Viewer data folder
     private fun getPicFiles() {
-        picFile = File(
+        picFileFull = File(
             Constants.STORAGE_FOLDER,
             "${teamNumber}_full_robot.jpg"
         )
+        picFileFront = File(
+            Constants.STORAGE_FOLDER,
+            "${teamNumber}_front.jpg"
+        )
+        picFileSide = File(
+            Constants.STORAGE_FOLDER,
+            "${teamNumber}_side.jpg"
+        )
     }
 
+    // Displays a robot picture for each picture added to the bitmap
     private fun showPictures(root: View) {
+        val pictureFilesList = listOf(picFileFull, picFileFront, picFileSide)
         val bitmapsToDisplay = mutableListOf<Bitmap>()
-        if (picFile!!.exists()) {
-            bitmapsToDisplay.add(BitmapFactory.decodeFile(picFile!!.absolutePath).rotated())
+
+        for (file in pictureFilesList) {
+            if (file!!.exists()) {
+                bitmapsToDisplay.add(BitmapFactory.decodeFile(file!!.absolutePath).rotated())
+            }
         }
+
         for (bitmap in bitmapsToDisplay) {
             root.ll_robot_pics.addView(ImageView(context).also {
                 it.scaleType = ImageView.ScaleType.FIT_START
@@ -64,6 +86,7 @@ class RobotPicFragment : Fragment() {
         }
     }
 
+    // Rotates the robot pictures to be displayed correctly
     private fun Bitmap.rotated() = Bitmap.createBitmap(
         this, 0, 0, width, height, Matrix().also { it.postRotate(90F) }, true
     )
